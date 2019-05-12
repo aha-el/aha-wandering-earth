98b9ddfb416007ac75fcce675149e0d050afea22
Ezylryb <46624975+Ezylryb-BK@users.noreply.github.com>
2019-05-13 02:44:08 +0800

    yyy

---
+++ js/main.js
@@ -0,0 +1,30 @@
+import run from './gen'
+
+let isRunning = false;
+
+const init = () =>  {
+  listen()
+  play();
+}
+
+function listen () {
+  document.getElementById('head').addEventListener('click', function() {
+    if (isRunning) return
+    play()
+  })
+}
+
+function play () {
+  const head = document.getElementById('head')
+  const history = run('AHA ! Wondering Earth !').history
+  isRunning = true;
+  let i = 0;
+  history.forEach((text, i) => {
+    setTimeout(function () {
+      head.innerText = text
+      if (++i === history.length) isRunning = false;
+    }, i * 30)
+  })
+}
+
+init()
